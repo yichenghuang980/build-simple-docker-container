4b50d713a6f3a01e70a540ec2b7536cfe7ad1945
Wilson Huang <yichenghuang980@gmail.com>
2021-03-06 19:50:25 +0000

    finalize docker conatiner project

--- app.py
+++ app.py
@@ -2,9 +2,24 @@
 import click
 
 @click.command()
-@click.option("--name")
-def hello(name):
-    click.echo(f'Hello {name}!')
+@click.option("--words")
+def hello(words): 
+
+    from google.cloud import language_v1
+
+    # Instantiates a client
+    client = language_v1.LanguageServiceClient()
+
+    # The text to analyze
+    text = words
+    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+
+    print("Text: {}".format(text))
+    print("Sentiment: {}".format(sentiment.score)) 
+    #click.echo(f'Hello {name}!')
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
